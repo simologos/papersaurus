58055e27542ec467460eece27d0f86b48f2f9b74
Simon Egli <simon.egli@bucher-suter.com>
2020-10-19 21:58:31 +0200

    Add jest to execute unit tests

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const path = require('path');
+
+const ignorePatterns = [
+  '/node_modules/',
+  '__fixtures__',
+  '/packages/docusaurus/lib',
+  '/packages/docusaurus-utils/lib',
+  '/packages/docusaurus-plugin-content-blog/lib',
+  '/packages/docusaurus-plugin-content-docs/lib',
+  '/packages/docusaurus-plugin-content-pages/lib',
+];
+
+module.exports = {
+  rootDir: path.resolve(__dirname),
+  verbose: true,
+  testURL: 'http://localhost/',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ignorePatterns,
+  coveragePathIgnorePatterns: ignorePatterns,
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  }
+};
